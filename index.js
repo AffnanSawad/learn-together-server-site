9dff2b5599ab39b7fcbfe0d7aa2eec214288687b
Affnan Sawad <affnansawad2002@gmail.com>
2024-08-25 16:17:30 +0600

    jwt added successfully

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser= require('cookie-parser');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
 
 // middleware
-app.use(cors())
+app.use(cors( {
+    origin:[
+      'http://localhost:5173',
+     
+    ],
+    credentials:true
+  } ))
+
 app.use(express.json())
+app.use(cookieParser());
 
 
 
@@ -31,6 +41,62 @@ const client = new MongoClient(uri, {
   }
 });
 
+  
+  
+  //  middleware for JWT
+  const logger = (req,res,next)=>{
+  
+    console.log('log:log info',req.method , req.url );
+    next();
+  }
+  
+  // 
+  const verifyToken = (req,res,next)=>{
+  
+    const token = req?.cookies?.token;
+  
+    console.log( 'token in the miidlleware' ,token);
+  
+    // no token
+    if(!token){
+  
+      return res.status(401).send( {message: 'Invalid Authirization'} )
+    }
+    // if token available
+  
+    jwt.verify(token, process.env.Access_Token_Secret, (err,decoded)=>{
+      
+  
+      if(err){
+  
+        return res.status(401).send( {message: 'Invalid Authirization'} )
+      }
+  
+      req.user = decoded;
+      next();
+  
+  
+    } )
+  
+    // next();
+  }
+  
+  // cookie option
+  const cookieOption={
+    
+    httpOnly:true,
+    secure:process.env.NODE_ENV === "production" ? true : false ,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict" ,
+  
+  
+  }
+
+
+
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -39,6 +105,39 @@ async function run() {
     // server
     const database = client.db("db_user");
     const creatingCollection = database.collection("creating");
+
+
+// JWT RELATED API
+  // jwt related 
+  app.post('/jwt',logger, async(req,res)=>{
+
+    const user = req.body;
+    console.log('token for user',user);
+
+    const token = jwt.sign(user, process.env.Access_Token, {expiresIn:'1h'} );
+
+    res.cookie('token',token , cookieOption)
+    .send({success:true});
+
+  } )
+
+
+  // cookie =>logOut
+  app.post('/logout',async(req,res)=>{
+
+    const user = req.body;
+    console.log('logging out' ,user);
+    
+    res.clearCookie( 'token', {...cookieOption , maxAge: 0 } ).send({success:true} )
+
+
+  } )
+
+
+
+
+
+    // feature card api .. CRUD STARTS HERE
       
     // post
     app.post("/creating", async(req,res)=>{
@@ -55,8 +154,9 @@ async function run() {
 
     // get
     app.get("/creating", async(req,res)=>{
-
          
+        
+           
         const cursor = creatingCollection.find();
         const result = await cursor.toArray();
         res.send(result);
